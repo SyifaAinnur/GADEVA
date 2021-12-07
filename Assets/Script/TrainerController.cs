a80b1c4e11f1187ebcf67c6949d2884def146b3e
Syifa Ainnur <arwahitem01@gmail.com>
2021-12-07 12:29:18 +0700

    memperbarui asset

---
+++ Assets/Script/TrainerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TrainerController : MonoBehaviour, Interactable
+{
+    [SerializeField] Dialog dialog;
+    Character character;
+
+    public void Interact(Transform initiator)
+    {
+        character.LookTowards(initiator.position);
+
+        StartCoroutine(DialogManager.Instance.ShowDialog(dialog, () =>
+        {
+            SceneManager.LoadScene("main");
+
+            
+        }));
+    }
+
+    private void Awake()
+    {
+        character = GetComponent<Character>();
+    }
+
+
+}
